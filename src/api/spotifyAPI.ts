93506fa1cbaa8e9517c9998eee6730ac033b540f
Bukky Oluseye <bukky.oluseye04@gmail.com>
2023-12-18 21:05:01 +0000

    ci: debug spotify api

--- src/api/spotifyAPI.ts
+++ src/api/spotifyAPI.ts
@@ -8,52 +8,63 @@ const clientSecret = process.env.GATSBY_SPOTIFY_CLIENT_SECRET;
 const refreshToken = process.env.GATSBY_SPOTIFY_REFRESH_TOKEN;
 
 const getAccessToken = async (): Promise<any> => {
-  const params = new URLSearchParams({
-    grant_type: "refresh_token",
-    refresh_token: `${refreshToken}`,
-    client_id: `${clientId}`,
-    client_secret: `${clientSecret}`,
-  });
-
-  const basic = btoa(`${clientId}:${clientSecret}`);
-
-  const response = await axios.post(TOKEN_ENDPOINT, params, {
-    headers: {
-      Authorization: `Basic ${basic}`,
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-  });
-
-  return await response.data.access_token;
-};
-
-export const getCurrentlyPlayingTrack = async (): Promise<any> => {
   try {
-    const accessToken = await getAccessToken();
+    const params = new URLSearchParams({
+      grant_type: "refresh_token",
+      refresh_token: `${refreshToken}`,
+      client_id: `${clientId}`,
+      client_secret: `${clientSecret}`,
+    });
 
-    const response = await axios.get(NOW_PLAYING_ENDPOINT, {
+    const basic = btoa(`${clientId}:${clientSecret}`);
+
+    const response = await axios.post(TOKEN_ENDPOINT, params, {
       headers: {
-        Authorization: `Bearer ${accessToken}`,
+        Authorization: `Basic ${basic}`,
+        "Content-Type": "application/x-www-form-urlencoded",
       },
     });
 
-    const song = response.data;
-    const albumImageUrl = song.item.album.images[0].url;
-    const artist = song.item.artists
-      .map((_artist: { name: any }) => _artist.name)
-      .join(", ");
-    const isPlaying = song.is_playing;
-    const songUrl = song.item.external_urls.spotify;
-    const title = song.item.name;
-
-    return {
-      albumImageUrl,
-      artist,
-      isPlaying,
-      songUrl,
-      title,
-    };
+    return await response.data.access_token;
   } catch (error) {
+    console.error("Error fetching currently playing track:", error);
     return null;
   }
 };
+
+export const getCurrentlyPlayingTrack = async (): Promise<any> => {
+  const accessToken = await getAccessToken();
+  if (accessToken !== null) {
+    try {
+      const response = await axios.get(NOW_PLAYING_ENDPOINT, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      const song = response.data;
+      console.log(song);
+      const albumImageUrl = song.item.album.images[0].url;
+      const artist = song.item.artists
+        .map((_artist: { name: any }) => _artist.name)
+        .join(", ");
+      const isPlaying = song.is_playing;
+      const songUrl = song.item.external_urls.spotify;
+      const title = song.item.name;
+
+      return {
+        albumImageUrl,
+        artist,
+        isPlaying,
+        songUrl,
+        title,
+      };
+    } catch (error) {
+      console.error("Error fetching currently playing track:", error);
+      return null;
+    }
+  } else {
+    console.log("error");
+  }
+};
+
