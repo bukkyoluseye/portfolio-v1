daca83cfb97d86c5b8efbc204749b219d7fd0685
Bukky Oluseye <bukky.oluseye04@gmail.com>
2024-01-13 21:19:57 +0000

    fix: clean up code and make backgroundColor a dependency for the useEffect hook

--- src/components/MiniPlayer/index.tsx
+++ src/components/MiniPlayer/index.tsx
@@ -21,9 +21,9 @@ const MiniPlayer = () => {
       setResult(currentlyPlaying);
       setLoading(false);
     };
-    
+
     const getTextColor = (background: string) => {
-      // const threshold = 4.5; // WCAG 2.0 AA standard for normal text
+      // The range for color difference
       const colorDifferenceThreshold = 500;
 
       const rgbStringToArray = (rgbString: string) => {
@@ -35,60 +35,18 @@ const MiniPlayer = () => {
         return rgbValues;
       };
 
+      // Call rgbStringToArray function with the rgbString passed through the getTextColor function
       const backgroundRGB = rgbStringToArray(background);
 
-      console.log(background, "background");
-
+      // Color difference is determined by the following formula:
       const colorDifference =
         Math.max(backgroundRGB[0], 255) -
         Math.min(backgroundRGB[0], 255) +
         (Math.max(backgroundRGB[1], 255) - Math.min(backgroundRGB[1], 255)) +
         (Math.max(backgroundRGB[2], 255) - Math.min(backgroundRGB[2], 255));
 
-      console.log(colorDifference, "color difference");
-
       // Determine text color based on the calculated contrast ratio
       return colorDifference >= colorDifferenceThreshold ? "#fff" : "#000";
-      //     Color brightness is determined by the following formula:
-      // ((Red value X 299) + (Green value X 587) + (Blue value X 114)) / 1000
-      // Note: This algorithm is taken from a formula for converting RGB values to YIQ values. This brightness value gives a perceived brightness for a color.
-
-      // Color difference is determined by the following formula:
-      // (maximum (Red value 1, Red value 2) - minimum (Red value 1, Red value 2)) + (maximum (Green value 1, Green value 2) - minimum (Green value 1, Green value 2)) + (maximum (Blue value 1, Blue value 2) - minimum (Blue value 1, Blue value 2))
-
-      // The rage for color brightness difference is 125. The range for color difference is 500.
-
-      // // Helper function to calculate contrast ratio
-      // const calculateContrastRatio = (color1: number[], color2: string) => {
-      //   const luminance1 = calculateRelativeLuminance(color1);
-      //   const luminance2 = calculateRelativeLuminance(color2);
-
-      //   const brighter = Math.max(luminance1, luminance2);
-      //   const darker = Math.min(luminance1, luminance2);
-
-      //   return (brighter + 0.05) / (darker + 0.05);
-      // };
-
-      // // Helper function to calculate relative luminance
-      // const calculateRelativeLuminance = (color: number[] | string) => {
-      //   const sRGB = color.map((value: number) => {
-      //     const sRGBValue = value / 255;
-      //     return sRGBValue <= 0.03928
-      //       ? sRGBValue / 12.92
-      //       : Math.pow((sRGBValue + 0.055) / 1.055, 2.4);
-      //   });
-
-      //   return 0.2126 * sRGB[0] + 0.7152 * sRGB[1] + 0.0722 * sRGB[2];
-      // };
-
-      // // Calculate contrast ratio with black text
-      // const contrastWithBlack = calculateContrastRatio(
-      //   rgbStringToArray(background),
-      //   "#000"
-      // );
-
-      // // Determine text color based on the calculated contrast ratio
-      // return contrastWithBlack >= threshold ? "#000" : "#fff";
     };
 
     const extractImageColor = () => {
@@ -98,15 +56,13 @@ const MiniPlayer = () => {
       // Ensure the image is loaded before trying to get the color
       if (albumArtImg && albumArtImg.complete) {
         const result = colorThief.getColor(albumArtImg, 25);
-        // Do something with the color result
-        console.log("Image color:", result);
+        // Convert dominant color from colorThief into an RGB string
         const rgbColor = `rgb(${result[0]}, ${result[1]}, ${result[2]})`;
         setBackgroundColor(rgbColor);
         let newTextColor;
         if (backgroundColor !== undefined) {
           newTextColor = getTextColor(backgroundColor);
         }
-        console.log(backgroundColor);
         backgroundColor !== undefined && setTextColor(newTextColor);
       }
     };
@@ -123,7 +79,7 @@ const MiniPlayer = () => {
     return () => {
       clearInterval(intervalId);
     };
-  }, []); // Empty dependency array to run the effect only once
+  }, [backgroundColor]); // Set background color as a dependency for the useEffect
 
   // const data = useStaticQuery(graphql`
   //   query {
